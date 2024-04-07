8e8fac47e019e46802f9ecd0c4a7c08e2db52b7e
Gana4225 <ch.ganapathi225@gmail.com>
2024-04-07 19:24:31 +0530

    database is connected

--- Regsuc.php
+++ Regsuc.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="rgsphp.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="con">
+        <div class="container">
+        <div class="jk">
+    
 <?php
 $servername = "localhost";
 $username = "gana225";
@@ -19,9 +32,21 @@ if($pwd == $rpwd)
 {
 $q="insert into register values('$acno','$un','$ifsc','$pwd','$rpwd')";
 $conn->query($q);
+
+echo "<u><h1>Your registeration process is succesful </h1></u><br>";
+echo("<h3>Your account number is:$acno</h3><br>");
+echo("<h3>Your username is:$un</h3><br>");
+echo("<h3>Your password is:$pwd</h3>");
+
 }
 else{
     echo("password does not match");
 }
 $conn->close();
 ?>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
+
---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+$servername = "localhost";
+$username = "gana225";
+$password = "gana225";
+$dbname ="bank";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$un=$_POST['un'];
+$pwd=$_POST['pwd'];
+$q="select username from register where username='$un'";
+$res=$conn->query($q);
+$row = $res->fetch_assoc();
+$nm=$row['username'];
+
+$q1="select password from register where password='$pwd'";
+$res1=$conn->query($q1);
+$row1 = $res1->fetch_assoc();
+$nm1=$row1['password'];
+if($un==$nm)
+{
+   if($pwd==$nm1){
+    header("Location:transaction.html");
+    exit();
+   }
+   else{
+    echo "incorrect password";
+   }
+}
+else{
+    echo "incorrect username";
+}
+
