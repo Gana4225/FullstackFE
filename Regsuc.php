59b9bb17b236c911aff2219015c2a2f4684703ca
Gana4225 <ch.ganapathi225@gmail.com>
2024-04-07 14:43:10 +0530

    php is modified

--- Regsuc.php
+++ Regsuc.php
@@ -1,3 +1,27 @@
 <?php
+$servername = "localhost";
+$username = "gana225";
+$password = "gana225";
+$dbname ="bank";
 
-        
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$acno=$_POST['acno'];
+$un=$_POST['un'];
+$ifsc=$_POST['ifsc'];
+$pwd=$_POST['pwd'];
+$rpwd=$_POST['rpwd'];
+if($pwd == $rpwd)
+{
+$q="insert into register values('$acno','$un','$ifsc','$pwd','$rpwd')";
+$conn->query($q);
+}
+else{
+    echo("password does not match");
+}
+$conn->close();
+?>
